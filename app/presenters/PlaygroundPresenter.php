d3f9d8ad1665dfc1138df5dffca35eee17a5d768
FSC <f.schad@era.aero>
2017-12-25 20:57:02 +0100

    Přidané presentery a šablony.

---
+++ app/presenters/PlaygroundPresenter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+
+
+class PlaygroundPresenter extends Nette\Application\UI\Presenter
+{
+}
---
+++ app/presenters/RulesPresenter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+
+
+class RulesPresenter extends Nette\Application\UI\Presenter
+{
+}
